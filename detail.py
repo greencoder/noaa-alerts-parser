a586b66ff1421ef651329aa02071a439c35eb2f7
Scott Newman <snewman18@gmail.com>
2013-09-05 22:52:09 -0600

    Added detail html page and generator for alerts.

---
+++ detail.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import json
+import codecs
+import datetime
+import pytz
+from operator import itemgetter
+
+from jinja2 import Template, Environment, FileSystemLoader
+
+CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+
+json_filepath = os.path.join(CUR_DIR, 'output/alerts.json')
+f = codecs.open(json_filepath, 'r', 'utf-8')
+contents = f.read()
+f.close()
+
+alert_data = json.loads(contents)
+alerts = alert_data['alerts']
+
+# Prepare to render the alerts
+env = Environment()
+env.loader = FileSystemLoader(os.path.join(CUR_DIR, 'templates'))
+
+now_utc = datetime.datetime.now(pytz.utc).astimezone(pytz.utc)
+
+template = env.get_template('detail.tpl.html')
+output = template.render(alerts=alerts, written_at_utc=now_utc)
+
+output_filepath = os.path.join(CUR_DIR, 'output/detail.html')
+f = codecs.open(output_filepath, 'w', 'utf-8')
+f.write(output)
+f.close()
+
